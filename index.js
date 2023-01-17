e401b1e67edf501c47108c408ec184230bb2f36f
hungnm <hungnm@unicloud.com.vn>
2023-01-17 16:28:06 +0700

    fix

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const bodyParser = require("body-parser");
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+app.post('/',(req,res)=>{
+    console.log('====================================');
+    console.log(req.body, req.params, req.query);
+    console.log('====================================');
+
+    res.json(req.body)
 })
 app.listen(process.env.PORT || 3000)
